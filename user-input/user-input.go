5dc11e93b18d33dc6e189970422b428cb56697b7
Hajime Suzuki <hajime.doublebass@gmail.com>
2021-09-21 22:14:50 +0200

    implement GetUserInput

--- user-input/user-input.go
+++ user-input/user-input.go
@@ -1,14 +1,45 @@
 package userinput
 
-import "top-down-tdd/abstractions"
+//go:generate mockgen -destination=./mocks/terminal.go -package=mocks top-down-tdd/user-input TerminalUtil
+
+import (
+	"fmt"
+	"top-down-tdd/abstractions"
+)
 
 type UserInputHandler struct {
+	terminal  TerminalUtil
+	presenter abstractions.Presenter
 }
 
-func NewUserInput() abstractions.InputHandler {
-	return UserInputHandler{}
+func NewUserInput(t TerminalUtil, p abstractions.Presenter) abstractions.InputHandler {
+	return UserInputHandler{t, p}
 }
 
 func (u UserInputHandler) GetUserInput(message string) string {
-	return "TODO: IMPLEMENT"
+	u.presenter.Dispay(message)
+	var input string
+	_, err := u.terminal.GetInput(&input)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return input
+}
+
+// this is used for mock
+type TerminalUtil interface {
+	GetInput(*string) (int, error)
+}
+
+type terminalUtil struct {
+}
+
+func (t terminalUtil) GetInput(a *string) (n int, err error) {
+	return fmt.Scanln(a)
+}
+
+func NewTerminalUtil() TerminalUtil {
+	return terminalUtil{}
 }
