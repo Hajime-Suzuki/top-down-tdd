2c75897780107be5daa14079e3c22f689e8612ec
Hajime Suzuki <hajime.doublebass@gmail.com>
2021-09-23 22:24:50 +0200

    add players test for GetCurrentPlayer

---
+++ players/players_suite_test.go
@@ -0,0 +1,53 @@
+package players
+
+import (
+	"testing"
+	"top-down-tdd/abstractions"
+	"top-down-tdd/abstractions/mocks"
+
+	"github.com/golang/mock/gomock"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestPlayers(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Players Suite")
+}
+
+func newCtrl() *gomock.Controller {
+	return gomock.NewController(GinkgoT())
+}
+
+var _ = Describe("Players", func() {
+
+	var (
+		mockCtrl *gomock.Controller
+	)
+
+	BeforeEach(func() {
+		mockCtrl = newCtrl()
+	})
+
+	Context("Players", func() {
+		Context("GetCurrentPlayer", func() {
+			It("return current player", func() {
+				// given
+				p1 := mocks.NewMockPlayer(mockCtrl)
+				p1.EXPECT().ShowName().Return("p1")
+				p2 := mocks.NewMockPlayer(mockCtrl)
+				ps := []abstractions.Player{
+					p1, p2,
+				}
+
+				// when
+				subject := Players{ps}
+
+				// then
+				res := subject.GetCurrentPlayer()
+				Expect(res.ShowName()).To(Equal("p1"))
+			})
+		})
+	})
+
+})
