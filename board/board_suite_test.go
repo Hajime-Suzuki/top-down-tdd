b649d5671a08a5c15fb532658276563f6142fc14
Hajime Suzuki <hajime.doublebass@gmail.com>
2021-09-25 13:36:30 +0200

    add more test cases for IsOver

--- board/board_suite_test.go
+++ board/board_suite_test.go
@@ -33,22 +33,90 @@ var _ = Describe("Board", func() {
 7 8 x 
 `))
 		})
+
 	})
+
 	Context("IsOver", func() {
-		It("returns true if there are 3 marks horizontally", func() {
-			// given
-			data := [][]string{
-				{"x", "x", "x"},
-				{"-", "o", "-"},
-				{"-", "-", "x"},
-			}
-			b := board{data}
+		When("Game is not over", func() {
+			It("return false 1", func() {
 
-			// when
-			subject := b.IsOver()
+				// given
+				data := [][]string{
+					{"-", "-", "-"},
+					{"-", "-", "-"},
+					{"-", "-", "-"},
+				}
+				b := board{data}
 
-			// then
-			Expect(subject).To(Equal(true))
+				// when
+				subject := b.IsOver()
+
+				// then
+				Expect(subject).To(Equal(false))
+			})
+			It("return false 2", func() {
+
+				// given
+				data := [][]string{
+					{"x", "x", "o"},
+					{"x", "-", "-"},
+					{"o", "o", "x"},
+				}
+				b := board{data}
+
+				// when
+				subject := b.IsOver()
+
+				// then
+				Expect(subject).To(Equal(false))
+			})
+		})
+		When("Game is over", func() {
+			It("return true if there are 3 marks horizontally (first row)", func() {
+				// given
+				data := [][]string{
+					{"x", "x", "x"},
+					{"-", "o", "-"},
+					{"-", "-", "x"},
+				}
+				b := board{data}
+
+				// when
+				subject := b.IsOver()
+
+				// then
+				Expect(subject).To(Equal(true))
+			})
+			It("return true if there are 3 marks horizontally (second row)", func() {
+				// given
+				data := [][]string{
+					{"o", "-", "x"},
+					{"o", "o", "o"},
+					{"-", "-", "x"},
+				}
+				b := board{data}
+
+				// when
+				subject := b.IsOver()
+
+				// then
+				Expect(subject).To(Equal(true))
+			})
+			It("return true if there are 3 marks horizontally (third row)", func() {
+				// given
+				data := [][]string{
+					{"o", "-", "x"},
+					{"-", "o", "o"},
+					{"x", "x", "x"},
+				}
+				b := board{data}
+
+				// when
+				subject := b.IsOver()
+
+				// then
+				Expect(subject).To(Equal(true))
+			})
 		})
 	})
 })
