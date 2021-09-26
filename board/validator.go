85a9d8f922155efa8cfd53b5d39739a406fde0a0
Hajime Suzuki <hajime.doublebass@gmail.com>
2021-09-26 15:09:03 +0200

    update validator

--- board/validator.go
+++ board/validator.go
@@ -9,19 +9,19 @@ import (
 
 func validate(board [][]string, position string) error {
 	p, _ := strconv.Atoi(position)
-	pf := float64(p)
-	if pf > BOARD_SIZE*BOARD_SIZE {
-		return errors.New("position is too big")
+	pos := float64(p)
+	if pos > BOARD_SIZE*BOARD_SIZE {
+		return errors.New("Position is too big")
 	}
 
-	row := math.Floor(pf / BOARD_SIZE)
-	col := math.Mod(pf, row*BOARD_SIZE)
+	row := math.Floor((pos - 1) / BOARD_SIZE)
+	col := math.Mod((pos - 1), row*BOARD_SIZE)
 
 	fmt.Println(row, col)
-	markInPos := board[int(row)][int(col)]
+	targetMark := board[int(row)][int(col)]
 
-	if !isEmpty(markInPos) {
-		return errors.New(fmt.Sprintf("the position %s is not empty", markInPos))
+	if !isEmpty(targetMark) {
+		return errors.New(fmt.Sprintf("Position %s is not empty", position))
 	}
 
 	return nil
